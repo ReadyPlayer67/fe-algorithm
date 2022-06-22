a1c237853892a4352c7f4e0c5605fe4d3c3d6781
liuqi <735611642@qq.com>
2022-06-22 22:25:38 +0800

    补充移除链表元素 && 环形链表笔记

--- leetcode/141.环形链表.js
+++ leetcode/141.环形链表.js
@@ -1,28 +1,43 @@
-/**
+/** 验证一个链表是否是环形链表（如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达）
  * @param {ListNode} head
  * @return {boolean}
  */
 const hasCycle = function(head) {
     //使用一个备忘录集合
-    // let cache = new Set()
-    // let p = head
-    // while(p){
-    //     if(cache.has(p)){
-    //         return true
-    //     }else{
-    //         cache.add(p)
-    //         p = p.next
-    //     }
-    // }
-    // return false
+    let cache = new Set()
+    let p = head
+    //从head开始遍历链表
+    while(p){
+        //如果cache中存在了该节点，代表通过指针再次到达了该节点，满足环形链表，return true
+        if(cache.has(p)){
+            return true
+        }else{
+            //否则在cache中添加该节点，继续往后遍历
+            cache.add(p)
+            p = p.next
+        }
+    }
+    //遍历完成后也没有发现环，就返回false
+    return false
+
+};
+
+//更好的方法，利用双指针（快慢指针）
+function hasCycle2(head){
+    //设置两个指针，初始都指向head
     let fast = head
     let slow = head
+    //如果不存在环，快指针会先移动到链表尾部，这时候需要终止循环
     while(fast && fast.next){
+        //快指针每次前进两个节点
         fast = fast.next.next
+        //慢指针每次前进一个节点
         slow = slow.next
+        //如果链表存在环，快指针一定会再次追上慢指针（相当于跑步套了一圈），这时候返回true
         if(fast === slow){
             return true
         }
     }
+    //没有环，循环结束，返回false
     return false
-};
+}
--- leetcode/203.移除链表元素.js
+++ leetcode/203.移除链表元素.js
@@ -5,22 +5,35 @@
  */
 const removeElements = function(head, val) {
     //第一种方法，利用递归
-    // if(head === null){
-    //     return head
-    // }
-    // head.next = removeElements(head.next,val)
-    // return head.val === val ? head.next : head
+    //如果节点是null，代表迭代已经完成了
+    if(head === null){
+        return head
+    }
+    //利用递归其实是从后往前去遍历这个链表
+    head.next = removeElements(head.next,val)
+    //如果当前节点是要被移除的，就返回节点后面的节点head.next,否则就返回节点本身，把这个返回的结果赋值给head.next就可以实现改变指针，移除节点
+    return head.val === val ? head.next : head
+};
+
+//第二种方法，通过遍历
+const removeElements2 = (head, val) => {
     //设置一个哨兵元素ele
+    //这样做的目的是把head也当成链表中间的一个普通节点，避免操作节点的时候每次都要判断该节点是否是head
     let ele = {
         next:head
     }
     let p = ele
+    //从head开始遍历链表
     while(p.next){
         if(p.next.val === val){
+            //如果是要删的节点，就改变指针，移除该节点
             p.next = p.next.next
         }else{
+            //否则就继续往后迭代
             p = p.next
         }
     }
+    //这里要注意，return的是ele.next而不是head
+    //因为我们从始至终一直都在操作ele.next，而没有操作head
     return ele.next
-};
+}
