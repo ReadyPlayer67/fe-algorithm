847eddfd00bdbf1eb72200664e1ddf202175621c
liuqi <735611642@qq.com>
2022-06-27 16:39:39 +0800

    利用回溯算法解决全排列问题

---
+++ leetcode/46.全排列.js
@@ -0,0 +1,38 @@
+/**
+ * 利用回溯算法解决全排列问题
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = function(nums) {
+    let list = []
+    //设计一个递归方法，填充list
+    backtrack(list,[],nums)
+    return list
+};
+
+//list是结果数组，temp是当前排列的结果，nums是源数据
+function backtrack(list,temp,nums){
+    //递归终止条件，如果当前排列了所有数字，说明找到了一个排列结果，push到list中
+    if(temp.length === nums.length){
+        list.push([...temp])
+    }
+    //从nums的第1个元素开始遍历，填到temp中
+    for(let i = 0;i<nums.length;i++){
+        //这里要注意不能填入重复的数字
+        if(temp.includes(nums[i])){
+            continue
+        }
+        //将数字填充到temp中
+        temp.push(nums[i])
+        //递归执行backtrack temp: []->[1]->[1,2]->[1,2,3]
+        backtrack(list,temp,nums)
+        //当找到一个排列结果后会退出递归，此时执行回溯，从temp中弹出一个元素temp:[1,2,3]->[1,2]
+        //此时这个backtrack方法循环i=2，循环结束，也会退出递归，继续弹出元素temp:[1,2]->[1]
+        //此时这个backtrack方法循环i=1，继续循环，找到下一个元素3，push到temp中,temp:[1]->[1,3]
+        //继续递归，找到temp中不存在的nums[i]是2，temp:[1,3]->[1,3,2]
+        //这样下一个解就找到了，依此不断地进行回溯
+        temp.pop()
+    }
+}
+
+console.log(permute([1,2,3]))
