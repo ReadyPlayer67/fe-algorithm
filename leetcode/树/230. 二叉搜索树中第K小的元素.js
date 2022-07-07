c988fcf8ed5ced003622de4630428766211519ee
liuqi <735611642@qq.com>
2022-07-07 23:51:01 +0800

    树刷题: 二叉搜索树中第K小的元素 && 二叉树的中序遍历 迭代解法

--- leetcode/树/230. 二叉搜索树中第K小的元素.js
+++ leetcode/树/230. 二叉搜索树中第K小的元素.js
@@ -6,11 +6,13 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
+/**求一个二叉搜索树中第k小的元素
  * @param {TreeNode} root
  * @param {number} k
  * @return {number}
  */
+//递归写法，用中序遍历将BST节点的值存到一个有序数组中，再获取数组k-1项
+//递归写法比较简单，但是也有缺点，他会遍历整颗树，但其实我们找到第k小的元素时即可退出遍历
 var kthSmallest = function(root, k) {
     let arr = []
     function dfs(node){
@@ -22,3 +24,25 @@ var kthSmallest = function(root, k) {
     dfs(root)
     return arr[k-1]
 };
+
+//迭代写法，在找到第k小的元素时可以直接退出遍历，节约时间
+function kthSmallest(root,k){
+    //类似94.二叉树的中序遍历，使用迭代方法遍历树
+    let count = 0
+    let stack = []
+    while(root || stack.length){
+        if(root){
+            stack.push(root)
+            root = root.left
+        }else{
+            let node = stack.pop()
+            //维护一个变量count，每次处理节点时count++
+            count++
+            //当count===k，代表找到了第k小的源，直接return
+            if(count === k){
+                return node.val
+            }
+            root = node.right
+        }
+    }
+}
--- leetcode/树/94. 二叉树的中序遍历.js
+++ leetcode/树/94. 二叉树的中序遍历.js
@@ -25,3 +25,28 @@ function walk(node,ret){
         walk(node.right,ret)
     }
 }
+
+//迭代写法，我们需要自己模拟递归的过程
+//递归的调用过程是不断往左边走，当左边走不下去了，就打印节点，并转向右边，然后右边继续这个过程。
+var inorderTraversal = function(root) {
+    let ret = []
+    let stack = []
+    //root有值代表root = root.left或root = node.right以后root依然有值，说明不是叶子节点，继续往下遍历，入栈
+    //root为null说明当前到了叶子节点，不需要再往下找了，该从栈弹出元素进行处理，处理完再处理该元素右侧节点
+    //如果root为null并且栈中无元素，说明树已经遍历完了
+    while(root || stack.length){
+        //不断往左子树方向走，每走一次就将当前节点保存到栈中
+        //这是模拟递归的调用
+        if(root){
+            stack.push(root)
+            root = root.left
+        }else{
+            //当前节点为空，说明左边走到头了，从栈中弹出节点并保存
+            //然后转向右边节点，继续上面整个过程（结合上面的walk方法一起理解）
+            let node = stack.pop()
+            ret.push(node.val)
+            root = node.right
+        }
+    }
+    return ret
+};
