a44f5300267e05646d3a391a377c33f9f28fad10
liuqi <735611642@qq.com>
2022-08-09 15:27:59 +0800

    K个一组翻转链表

---
+++ leetcode/链表/25. K 个一组翻转链表.js
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var reverseKGroup = function(head, k) {
+    //初始化一个哨兵节点，next为head
+    let dummy = {
+        val:null,
+        next:head
+    }
+    //需要翻转的子链表的前一个节点，记录下来，之后重新连接子链表的时候用
+    let pre = dummy
+    //遍历链表
+    while(head){
+        //tail记录当前翻转的子链表的最后一个节点
+        let tail = pre
+        //查看剩余部分长度是否大于等于k，小于k不用翻转了，直接返回链表
+        for(let i=0;i<k;i++){
+            tail = tail.next
+            if(!tail){
+                return dummy.next
+            }
+        }
+        //需要翻转的子链表的后一个节点，记录下来，之后重新连接子链表的时候用
+        let next = tail.next;
+        //翻转子链表
+        [head,tail] = myReverse(head,tail)
+        //把子链表拼回原来的链表 pre -> 3->2->1 ->next
+        pre.next = head
+        tail.next = next
+        //重新计算pre和head，进一步向后翻转
+        pre = tail
+        head = tail.next
+    }
+    return dummy.next
+};
+//翻转子链表的方法
+function myReverse(head,tail){
+    let prev = tail.next
+    let p = head
+    while(prev !== tail){
+        const next = p.next
+        p.next = prev
+        prev = p
+        p = next
+    }
+    //此时tail是翻转好的头节点，head是翻转好的链表的尾部节点
+    return [tail,head]
+}
