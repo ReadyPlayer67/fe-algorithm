6d6f18f83dca7d3dae35837dd4d0e5b5b27c94e0
liuqi <735611642@qq.com>
2022-08-16 11:25:54 +0800

    有序链表转换二叉搜索树

---
+++ 树/109. 有序链表转换二叉搜索树.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {TreeNode}
+ */
+var sortedListToBST = function(head) {
+    //分治+递归思想，我们每次找到链表的中间位置节点作为root节点
+    //并且将链表根据中间位置节点一分为二，对左右两部分链表递归处理即可
+    if(!head){
+        return null
+    }
+    //我这里偷懒了，将链表转换为了数组再取中间节点，消耗了额外的空间
+    //可以利用快慢指针获取链表中间节点
+    let list = []
+    while(head){
+        list.push(head.val)
+        head = head.next
+    }
+    //递归处理方法，取中点作为root节点，递归生成左右子树
+    function dfs(arr){
+        if(arr.length < 1){
+            return null
+        }
+        let mid = arr.length >> 1
+        let node = new TreeNode(arr[mid])
+        node.left = dfs(arr.slice(0,mid))
+        node.right = dfs(arr.slice(mid+1))
+        return node
+    }
+    return dfs(list)
+};
