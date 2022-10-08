fed8c86ded932d5bb1edc7177770ca4b68200bfa
liuqi <735611642@qq.com>
2022-10-08 22:41:46 +0800

    重复刷题 字母移位 && 搜索旋转数组

--- 字符串/_848. 字母移位.js
+++ 字符串/_848. 字母移位.js
@@ -9,7 +9,7 @@ var shiftingLetters = function(s, shifts) {
     //倒序遍历
     for(let i=shifts.length-1;i>=0;i--){
         //sum为每一位需要位移的距离
-        sum += shifts[i] % 26
+        sum += shifts[i]
         //假设'a'的位置为0，index为当前位字母位移后的下标
         let index = arr[i].charCodeAt()- 'a'.charCodeAt() + sum
         //index有可能大于26，对26取余后转换为字母，即当前位字母位移后的结果
